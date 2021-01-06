62c0cad3bfad9a402dc986ed24e5ba0f7169f8e5
jspannu919 <jspannu919@gmail.com>
2021-01-06 16:42:19 +0530

    User Info Tooltip Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,29 @@
 import { useSelector } from "react-redux";
 import { GoogleLogout } from "react-google-login";
+import { Tooltip } from "antd";
 import "./header.css";
 
 const Header = (props) => {
   const userObj = useSelector((state) => state.LoginReducer.userObj);
 
+  function getUserInfo() {
+    return (
+      <>
+        <span>Email: {userObj.email}</span>
+        <br />
+        <span>FullName: {userObj.name}</span>
+      </>
+    );
+  }
+
   return (
     <header>
-      <div className="userInfo">
-        <img src={userObj.imageUrl} alt="userPhoto" className="userImg" />
-        <span className="userName">Welcome {userObj.givenName}</span>
-      </div>
+      <Tooltip placement="bottomLeft" title={getUserInfo()}>
+        <div className="userInfo">
+          <img src={userObj.imageUrl} alt="userPhoto" className="userImg" />
+          <span className="userName">Welcome {userObj.givenName}</span>
+        </div>
+      </Tooltip>
       <GoogleLogout
         clientId={process.env.REACT_APP_CLIENT_ID}
         buttonText="Logout"
